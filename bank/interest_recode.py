97e322b468281adec8870ff8f27e00e039422210
Xing-Huang <hxg@palliums.org>
2020-12-10 15:04:55 +0800

    change get_interest

--- bank/interest_recode.py
+++ bank/interest_recode.py
@@ -20,12 +20,22 @@ class InterestRecords():
         self.records[start_time][1] -= amount
 
     def get_interest(self, total_borrow, borrow_rate, t):
-        self.update_total_borrow(total_borrow, borrow_rate, t)
         start_time = str(t // self.ONE_DAY)
+        value = self.records.get(start_time)
+        if value is None:
+            if len(self.records):
+                key = self.records.keys()[0]
+                if int(key) > int(start_time):
+                    return None
+                start_amount = self.get_start_total_borrow(total_borrow, borrow_rate, int(start_time)*self.ONE_DAY, t)
+                return total_borrow - start_amount
+            else:
+                return None
         start_amount, borrow_amount = self.records.get(start_time)
         return total_borrow - start_amount - borrow_amount
 
     @staticmethod
     def get_start_total_borrow(end_total_borrow, borrow_rate, start_time, end_time):
         return max(end_total_borrow - int((end_time-start_time) / 60) * borrow_rate, 0)
+
 
