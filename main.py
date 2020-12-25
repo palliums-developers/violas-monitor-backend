e963837a93a918539865b3157dddf7c729229058
Xing-Huang <hxg@palliums.org>
2020-12-25 17:36:38 +0800

    add swap tx

---
+++ main.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Test:
+    v1: list
+
+
+
+test = Test()
+print(test.v1)
+
--- swap/exchange.py
+++ swap/exchange.py
@@ -1,75 +1,85 @@
-import json
+from dataclasses import dataclass
+from violas_client.extypes.bytecode import CodeType
 
-from .exdep import Exdep
-from .volume import Volumes
+from db.base import Base
 
-class RegisteredCurrencies():
-    def __init__(self, currency_codes=None):
-        self.currency_codes = currency_codes or list()
 
-class Token():
-    def __init__(self, index, value):
-        self.index = index
-        self.value = value
+@dataclass
+class UserInfo:
+    amount: int
+    reward_debt: int
 
-    def __repr__(self):
-        return str(self.__dict__)
+@dataclass
+class PoolUserInfo:
+    pool_id: int
+    user_info: UserInfo
 
-class Reserve():
-    def __init__(self, coina, coinb, liquidity_total_supply=None):
-        self.liquidity_total_supply = liquidity_total_supply or 0
-        self.coina = coina
-        self.coinb = coinb
+@dataclass
+class PoolInfo:
+    id: int
+    lp_supply: int
+    alloc_point: int
+    acc_vls_per_share: int
 
-    def __repr__(self):
-        return str(self.__dict__)
+@dataclass
+class RewardPools:
+    start_time: int
+    end_time: int
+    last_reward_time: int
+    total_reward_balance: int
+    total_alloc_point: int
+    pool_infos: list
 
-class Exchange():
+@dataclass
+class Token:
+    index: int
+    value: int
+
+@dataclass
+class Reserve:
+    liquidity_total_supply: int
+    coina: Token
+    coinb: Token
+
+class ExchangeAPI(Base):
     def __init__(self):
-        self.reserves = list()
-        self.currency_codes = list()
-        #币种的24小时交易量
-        self.trading_recodes = dict()
-        #交易对的gas_fee
-        self.fees = dict()
+        # {currency_code: price}
         self.oracle_prices = dict()
+        # 手续费{pair_name: Volumes}
+        self.fees = dict()
+        # 交易量{currency: Volumes}
+        self.currency_trading_recodes = dict()
+        # 交易量{pair_name: Volumes}
+        self.pair_trading_recodes = dict()
+
+    def add_tx(self, tx):
+        code_type = tx.get_code_type()
+        if code_type == CodeType.INITIALIZE:
+            return self.add_initialize(tx)
+        elif code_type == CodeType.ADD_CURRENCY:
+            pass
+        elif code_type == CodeType.SWAP:
+            pass
+        elif code_type == CodeType.ADD_LIQUIDITY:
+            pass
+        elif code_type == CodeType.REMOVE_LIQUIDITY:
+            pass
+        elif code_type == CodeType.WITHDRAW_MINE_REWARD:
+            pass
+
+    def add_initialize(self, tx):
+        t = tx.get_swap_timestamp()
+        self.reward_pool = RewardPools(t, t, t, 0, 0, list())
+        self.reserves = list()
+        self.registered_currencies = list()
+        self.factor1 = 9997
+        self.factor2 = 10000
 
     def add_currency(self, tx):
-        currency_code = tx.get_currency_code()
-        if currency_code in self.currency_codes:
-            return
-        self.currency_codes.append(currency_code)
-        self.trading_recodes[currency_code] = Volumes()
+        currency = tx.get_currency_code()
+        self.registered_currencies.append(currency)
 
-    def add_liquidity(self, tx):
-        script = tx.get_script()
-        coina, coinb = script.type_arguments
-        amounta_desired, amountb_desired = script.arguments[0], script.arguments[1]
-        ida, idb = self.get_pair_indexes(coina, coinb)
-        reserve = self.get_reserve_internal(ida, idb)
-        total_supply, reservea, reserveb = reserve.liquidity_total_supply, reserve.coina.value, reserve.coinb.value
-
-        total_supply, amounta, amountb = self.mint(amounta_desired, amountb_desired, reservea, reserveb, total_supply)
-        reserve.liquidity_total_supply = total_supply
-        reserve.coina.value = reservea + amounta
-        reserve.coinb.value = reserveb + amountb
-
-    def remove_liquidity(self, tx):
-        script = tx.get_script()
-        coina, coinb = script.type_arguments
-        liquidity = script.arguments[0]
-
-        ida, idb = self.get_pair_indexes(coina, coinb)
-        reserve = self.get_reserve_internal(ida, idb)
-        total_supply, reservea, reserveb = reserve.liquidity_total_supply, reserve.coina.value, reserve.coinb.value
-
-        amounta = int(liquidity*reservea / total_supply)
-        amountb = int(liquidity*reserveb / total_supply)
-        reserve.liquidity_total_supply = total_supply - liquidity
-        reserve.coina.value = reservea - amounta
-        reserve.coinb.value = reserveb - amountb
-
-    def swap(self, tx):
+    def add_swap(self, tx):
         script = tx.get_script()
         args = script.get_args()
         path = args[3]
@@ -88,90 +98,48 @@ class Exchange():
             if id_in < id_out:
                 reserve = self.get_reserve_internal(id_in, id_out)
                 reserve_in, reserve_out = reserve.coina.value, reserve.coinb.value
-                amount_out = Exdep.get_amount_out(amt_in, reserve_in, reserve_out)
+                amount_out = self.get_amount_out(amt_in, reserve_in, reserve_out)
                 amounts.append(amount_out)
-                key = self.get_key(coin_in, coin_out)
-                fee = Exdep.get_fee(amt_in, reserve_in, reserve_out) * self.oracle_prices.get(coin_out)
-                self.fees.get(key).increase_value(fee, timestamp)
                 reserve.coina.value += amt_in
-                self.trading_recodes.get(coin_in).increase_value(amt_in, timestamp)
                 reserve.coinb.value -= amount_out
-                self.trading_recodes.get(coin_out).increase_value(amount_out, timestamp)
+
+                pair_name = self.get_pair_name(coin_in, coin_out)
+                fee = self.get_fee(amt_in, reserve_in, reserve_out) * self.oracle_prices.get(coin_out)
+                self.fees.get(pair_name).increase_value(fee, timestamp)
+                self.currency_trading_recodes.get(coin_in).increase_value(amt_in, timestamp)
+                self.currency_trading_recodes.get(coin_out).increase_value(amount_out, timestamp)
+                self.pair_trading_recodes.get(pair_name).increase_value(amt_in * self.oracle_prices.get(coin_in))
             else:
                 reserve = self.get_reserve_internal(id_out, id_in)
                 reserve_in, reserve_out = reserve.coinb.value, reserve.coina.value
-                amount_out = Exdep.get_amount_out(amt_in, reserve_in, reserve_out)
+                amount_out = self.get_amount_out(amt_in, reserve_in, reserve_out)
                 amounts.append(amount_out)
-                key = self.get_key(coin_out, coin_in)
-                fee = Exdep.get_fee(amt_in, reserve_in, reserve_out)
-                self.fees.get(key).increase_value(fee, timestamp)
                 reserve.coina.value -= amount_out
-                self.trading_recodes.get(coin_out).increase_value(amount_out, timestamp)
                 reserve.coinb.value += amt_in
-                self.trading_recodes.get(coin_in).increase_value(amt_in, timestamp)
+
+                pair_name = self.get_pair_name(coin_out, coin_in)
+                fee = self.get_fee(amt_in, reserve_in, reserve_out) * self.oracle_prices.get(coin_out)
+                self.fees.get(pair_name).increase_value(fee, timestamp)
+                self.currency_trading_recodes.get(coin_in).increase_value(amt_in, timestamp)
+                self.currency_trading_recodes.get(coin_out).increase_value(amount_out, timestamp)
+                self.pair_trading_recodes.get(pair_name).increase_value(amt_in * self.oracle_prices.get(coin_in))
             i += 1
 
-    def update_reserves_to_db(self):
-        if len(self.reserves) == 0:
-            return
-        values = ''
-        for reserve in self.reserves:
-            coina = self.get_coin_name(reserve.coina.index)
-            coinb = self.get_coin_name(reserve.coinb.index)
-            key = self.get_key(coina, coinb)
-            values += f'''('{key}', '{json.dumps(reserve.__dict__)}'),'''
-        values = values[:-1]
 
-        sql = f'''
-        INSERT INTO monitor (key, value) VALUES {values} ON CONFLICT (key) DO UPDATE
-        SET value=excluded.value;
-        '''
-        self.execute(sql)
+    def add_liquidity(self, tx):
+        pass
 
-    def update_interval_lock_to_db(self):
-        values = f"('interval_lock', '{json.dumps({self.interval_lock})}')"
-        sql = f'''
-        INSERT INTO monitor (key, value) VALUES {values} ON CONFLICT (key) DO UPDATE
-        SET value=exclude.value
-        '''
-        self.execute(sql)
+    def remove_liquidity(self, tx):
+        pass
 
-    def update_interval_borrow_to_db(self):
-        values = f"('interval_borrow', '{json.dumps({self.interval_borrow})}')"
-        sql = f'''
-        INSERT INTO monitor (key, value) VALUES {values} ON CONFLICT (key) DO UPDATE
-        SET value=exclude.value
-        '''
-        self.execute(sql)
+    def withdraw_mine_reward(self, tx):
+        pass
 
-    def update_to_db(self):
-        self.update_token_to_db()
-        self.update_interval_borrow_to_db()
-        self.update_interval_lock_to_db()
 
-    def update_from_db(self):
-        sql = "SELECT * FROM monitor"
-        values = self.query(sql)
-        for key, value in values.items():
-            if key == "interval_lock":
-                self.interval_lock = value
-            elif key == "interval_borrow":
-                self.interval_borrow = value
-            else:
-                self.token_infos[key] = value
+    '''................................called by internal..........................'''
 
-    '''...............................internal.................................'''
-
-    def get_coin_id(self, currency_code):
-        for index, code in enumerate(self.currency_codes):
-            if code == currency_code:
-                return index
-
-    def get_pair_id(self, coina, coinb):
-        return self.get_coin_id(coina), self.get_coin_id(coinb)
-
-    def get_pair_indexes(self, coina, coinb):
-        return self.get_coin_id(coina), self.get_coin_id(coinb)
+    def get_coin_name(self, id):
+        return self.registered_currencies[id]
 
     def get_reserve_internal(self, ida, idb):
         for reserve in self.reserves:
@@ -181,14 +149,26 @@ class Exchange():
         self.reserves.append(reserve)
         return reserve
 
-    def get_key(self, coina, coinb):
+    def get_amount_out(self, amount_in, reserve_in, reserve_out):
+        amount_in_with_fee = amount_in * self.factor1
+        numerator = amount_in_with_fee * reserve_out
+        denominator = reserve_in * self.factor2 + amount_in_with_fee
+        return int(numerator / denominator)
+
+    def get_pair_name(self, coina, coinb):
         return coina + "_" + coinb
 
-    def get_coin_name(self, id):
-        return self.currency_codes[id]
+    def get_output_amount_without_fee(self, amount_in, reserve_in, reserve_out):
+        amount_out = amount_in * reserve_out // (reserve_in + amount_in)
+        return amount_out
 
-    def mint(self, amounta_desired, amountb_desired, reservea, reserveb, total_supply):
-        return Exdep.get_mint_liquidity(amounta_desired, amountb_desired, reservea, reserveb, total_supply)
+    def get_output_amount(self, amount_in, reserve_in, reserve_out):
+        assert reserve_in > 0 and reserve_out > 0
+        amount_inWithFee = amount_in * self.factor1
+        numerator = amount_inWithFee * reserve_out
+        denominator = reserve_in * self.factor2 + amount_inWithFee
+        amount_out = numerator // denominator
+        return amount_out
 
-
-
+    def get_fee(self, amount_in, reserve_in, reserve_out):
+        return self.get_output_amount_without_fee(amount_in, reserve_in, reserve_out) - self.get_output_amount(amount_in, reserve_in, reserve_out)
